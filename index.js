3af4de93b978b326abaac3d1430ef383f9949912
Stefan Espant <info@espant.com>
2023-02-23 15:23:19 +0100

    v1.8
    fix

--- index.js
+++ index.js
@@ -1,47 +1,73 @@
-import express from 'express'
+import express from "express";
 
 // Haalt de API gegevens op uit de bovenste laag: de root.
-const url = 'https://whois.fdnd.nl/api/v1/'
+const url = "https://whois.fdnd.nl/api/v1/";
 
 // Creëert een nieuwe express app
-const app = express()
+const app = express();
 
 // Configureert hoe ik Express kan gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 // Maakt een route voor de index
+<<<<<<< HEAD
+app.get("/", async (request, response) => {
+  let { id, direction } = request.query;
+=======
 app.get('/', async (request, response) => {
-	let { id, direction } = request.query
+	let { id, direction, favoriteDrink } = request.query
+>>>>>>> 46744aea778e12cc1cba812075a8691273563b06
 
-	let squads
-	let squad
+  let squads;
+  let squad;
 
-	// Haalt alle squads op uit de API en wacht voordat voorgaande commando's zijn uitgevoerd
-	await getSquads()
-		.then((response) => squads = response)
+  // Haalt alle squads op uit de API en wacht voordat voorgaande commando's zijn uitgevoerd
+  await getSquads().then((response) => (squads = response));
 
-	// Filter uit de squads
-	squads = squads.filter((item) => item.slug.startsWith('squa'))
+  // Filter uit de squads
+  squads = squads.filter((item) =>
+    item.slug.startsWith("squa")
+  );
 
+<<<<<<< HEAD
+  if (!id) id = squads[2].id;
+  if (!direction) direction = "ASC";
+  await getSquad({
+    id,
+    orderBy: "surname",
+    direction,
+  }).then((response) => (squad = response));
+
+  response.render("index", {
+    squads,
+    squad,
+    members: squad.members,
+  });
+});
+=======
 	if (!id) id = squads[2].id
 	if (!direction) direction = 'ASC'
-	await getSquad({ id, orderBy: 'surname', direction })
+	await getSquad({ id, orderBy: 'surname', direction, favoriteDrink })
 		.then((response) => squad = response)
 
-	response.render('index', { squads, squad, members: squad.members })
+	response.render('index', { squads, squad, members: squad.members, favoriteDrink })
 })
+>>>>>>> 46744aea778e12cc1cba812075a8691273563b06
 
 // Configureert op welke poortnummer express naar luisteren zal
-app.set('port', process.env.PORT || 8000)
-
+app.set("port", process.env.PORT || 8000);
 
 // Lanceert Express & ontvangt de geconfigureerde poortnummer
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toont een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+  console.log(
+    `Application started on http://localhost:${app.get(
+      "port"
+    )}`
+  );
+});
 
 // JSON gegevens ophalen nadat ze zijn goedgekeurd
 
@@ -52,12 +78,27 @@ app.listen(app.get('port'), function () {
  * @returns Ophalen met geparseerde JSON-gegevens
  */
 async function fetchJson(endpoint, queryParams) {
+<<<<<<< HEAD
+  let queryParamsString = "";
+  if (queryParams)
+    queryParamsString = new URLSearchParams(
+      queryParams
+    ).toString();
+=======
 	let queryParamsString = ''
-	if (queryParams) queryParamsString = new URLSearchParams(queryParams).toString()
+	if (queryParams) {
+		for (const property in queryParams) {
+			if (typeof queryParams[property] === 'undefined') delete(queryParams[property])
+		}
+		queryParamsString = new URLSearchParams(queryParams).toString()
+	}
+>>>>>>> 46744aea778e12cc1cba812075a8691273563b06
 
-	return await fetch(`${ url }/${ endpoint }?${ queryParamsString }`)
-		.then((response) => response.json())
-		.catch((error) => error)
+  return await fetch(
+    `${url}/${endpoint}?${queryParamsString}`
+  )
+    .then((response) => response.json())
+    .catch((error) => error);
 }
 
 /**
@@ -66,10 +107,10 @@ async function fetchJson(endpoint, queryParams) {
  * @returns Array met squads
  */
 function getSquads() {
-	const endpoint = 'squads'
-	return fetchJson(endpoint)
-		.then((response) => response.squads)
-		.catch(() => undefined)
+  const endpoint = "squads";
+  return fetchJson(endpoint)
+    .then((response) => response.squads)
+    .catch(() => undefined);
 }
 
 /**
@@ -78,12 +119,11 @@ function getSquads() {
  * @returns Enkele squad
  */
 function getSquad(queryParams) {
-	const endpoint = 'squad'
-	return fetchJson(endpoint, queryParams)
-		.then((response) => response.squad)
-		.catch(() => undefined)
+  const endpoint = "squad";
+  return fetchJson(endpoint, queryParams)
+    .then((response) => response.squad)
+    .catch(() => undefined);
 }
 
 // Filteren van studenten op basis van voorkeur
-let drinks = ['coffee','tea','water']
-
+let drinks = ["coffee", "tea", "water"];
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,100 +1,128 @@
 // Sound effects loaded made with an array & strings
-import 'https://rawcdn.githack.com/flackr/scroll-timeline/ca40d920073ab1c3775f6e2aa973c6ea747ae664/dist/scroll-timeline.js';
+import "https://rawcdn.githack.com/flackr/scroll-timeline/ca40d920073ab1c3775f6e2aa973c6ea747ae664/dist/scroll-timeline.js";
 
 const coffeeAudio = [
-	new Audio('https://nichtrijder.pepijnblom.com/sounds/krijg%20nou%20een%20bakkie%20pleur.mp3'),
-	new Audio('./assets/audio/de-eerste-indruk-is-romig.mp3')
+  new Audio(
+    "https://nichtrijder.pepijnblom.com/sounds/krijg%20nou%20een%20bakkie%20pleur.mp3"
+  ),
+  new Audio("./assets/audio/de-eerste-indruk-is-romig.mp3"),
 ];
-const teaAudio = new Audio('./assets/audio/cup-of-tea.m4a')
-const waterAudio = new Audio('./assets/audio/water.m4a')
+const teaAudio = new Audio("./assets/audio/cup-of-tea.m4a");
+const waterAudio = new Audio("./assets/audio/water.m4a");
 
-const liquid = document.querySelector('.liquid')
-const dispenser = ['.left-dispenser-output', '.right-dispenser-output']
+const liquid = document.querySelector(".liquid");
+const dispenser = [
+  ".left-dispenser-output",
+  ".right-dispenser-output",
+];
 
-addEventListener('click', (event) => {
-	const { target } = event
+addEventListener("click", (event) => {
+  const { target } = event;
 
-	if (!target.matches('button')) return
+  if (!target.matches("button")) return;
 
-	switch (target.id) {
-		case 'coffeeEvent':
-			playCoffee()
-			break;
-		case 'teaEvent':
-			playTea()
-			break;
-		case 'waterEvent':
-			playWater()
-			break;
+  switch (target.id) {
+    case "coffeeEvent":
+      playCoffee();
+      break;
+    case "teaEvent":
+      playTea();
+      break;
+    case "waterEvent":
+      playWater();
+      break;
 
-		default:
-			break;
-	}
-})
-
+    default:
+      break;
+  }
+});
 
 function playCoffee() {
-	const randomIndex = Math.floor(Math.random() * coffeeAudio.length);
-	const randomAudio = coffeeAudio[randomIndex];
-	randomAudio.play();
-	setDispencerClass('coffee')
+  const randomIndex = Math.floor(
+    Math.random() * coffeeAudio.length
+  );
+  const randomAudio = coffeeAudio[randomIndex];
+  randomAudio.play();
+  setDispencerClass("coffee");
 }
 
 function playTea() {
-	teaAudio.play()
-	setDispencerClass('tea')
+  teaAudio.play();
+  setDispencerClass("tea");
 }
 
 function playWater() {
-	waterAudio.play()
-	setDispencerClass('water')
+  waterAudio.play();
+  setDispencerClass("water");
 }
 
 function setDispencerClass(drink) {
-	dispenser.forEach(dispenserSelector => {
-		const dispenserElement = document.querySelector(dispenserSelector)
-		dispenserElement.classList.add('liquid', drink)
-		setTimeout(() => dispenserElement.classList.remove('liquid', drink), 12000);
-	})
+  dispenser.forEach((dispenserSelector) => {
+    const dispenserElement = document.querySelector(
+      dispenserSelector
+    );
+    dispenserElement.classList.add("liquid", drink);
+    setTimeout(
+      () =>
+        dispenserElement.classList.remove("liquid", drink),
+      12000
+    );
+  });
 }
 
 function buildScrollPanel() {
-	const $listView = document.querySelector('.squad-members > ul');
-	const $listItems = document.querySelectorAll('.squad-members > ul > li');
+  const $listView = document.querySelector(
+    ".squad-members > ul"
+  );
+  const $listItems = document.querySelectorAll(
+    ".squad-members > ul > li"
+  );
 
-	$listItems.forEach(($listItem) => {
-		const timeline = new ViewTimeline({
-			source: $listView,
-			subject: $listItem,
-			axis: 'inline'
-		})
+  $listItems.forEach(($listItem) => {
+    const timeline = new ViewTimeline({
+      source: $listView,
+      subject: $listItem,
+      axis: "inline",
+    });
 
-		// Appear
-		$listItem.animate(
-			{
-				opacity: [0, 1],
-				transform: ['translateX(100%)', 'translateX(0)'],
-			},
-			{
-				timeline,
-				delay: { phase: 'enter', percent: CSS.percent(0) },
-				endDelay: { phase: 'enter', percent: CSS.percent(100) }
-			}
-		)
+    // Appear
+    $listItem.animate(
+      {
+        opacity: [0, 1],
+        transform: ["translateX(100%)", "translateX(0)"],
+      },
+      {
+        timeline,
+        delay: {
+          phase: "enter",
+          percent: CSS.percent(0),
+        },
+        endDelay: {
+          phase: "enter",
+          percent: CSS.percent(100),
+        },
+      }
+    );
 
-		// Disappear
-		$listItem.animate(
-			{
-				opacity: [1, 0],
-				transform: ['translateX(0%)', 'translateX(-100%)'],
-			},
-			{
-				timeline,
-				delay: { phase: 'exit', percent: CSS.percent(0) },
-				endDelay: { phase: 'exit', percent: CSS.percent(100) }
-			}
-		)
-	})
+    // Disappear
+    $listItem.animate(
+      {
+        opacity: [1, 0],
+        transform: ["translateX(0%)", "translateX(-100%)"],
+      },
+      {
+        timeline,
+        delay: {
+          phase: "exit",
+          percent: CSS.percent(0),
+        },
+        endDelay: {
+          phase: "exit",
+          percent: CSS.percent(100),
+        },
+      }
+    );
+  });
 }
 
-buildScrollPanel()
+buildScrollPanel();
